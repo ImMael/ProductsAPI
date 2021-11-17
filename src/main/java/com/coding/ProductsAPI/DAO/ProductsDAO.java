2f292e7ca2ebc96b644eed3dde1ad23c988135ab
ImMael <legrand.mael2002@gmail.com>
2021-11-17 10:15:15 +0100

    Add ProdController, DAO & Model

---
+++ src/main/java/com/coding/ProductsAPI/DAO/ProductsDAO.java
@@ -0,0 +1,7 @@
+package com.coding.ProductsAPI.DAO;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductsDAO {
+}
---
+++ src/main/java/com/coding/ProductsAPI/models/Products.java
@@ -0,0 +1,62 @@
+package com.coding.ProductsAPI.models;
+
+import java.sql.Date;
+
+public class Products {
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    private Integer categoryId;
+    private Integer id;
+    private String type;
+    private Integer rating;
+    private String name;
+    private Date createdAt;
+
+}
