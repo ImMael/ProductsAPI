4baeb199f53ab7073c85343eef1039f531473714
ImMael <legrand.mael2002@gmail.com>
2021-11-17 10:53:12 +0100

    /products working

--- src/main/java/com/coding/ProductsAPI/Controller/ProductsController.java
+++ src/main/java/com/coding/ProductsAPI/Controller/ProductsController.java
@@ -1,12 +1,23 @@
 package com.coding.ProductsAPI.Controller;
 
 import com.coding.ProductsAPI.DAO.ProductsDAO;
+import com.coding.ProductsAPI.models.Products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class ProductsController {
 
     @Autowired
     ProductsDAO productsService;
+
+    @RequestMapping("/products")
+    public @ResponseBody List<Products> index(){
+        return productsService.readAll();
+    }
 }
--- src/main/java/com/coding/ProductsAPI/DAO/ProductsDAO.java
+++ src/main/java/com/coding/ProductsAPI/DAO/ProductsDAO.java
@@ -1,7 +1,20 @@
 package com.coding.ProductsAPI.DAO;
 
+import com.coding.ProductsAPI.models.Products;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductsDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Products> readAll(){
+        return jdbcTemplate.query("SELECT * FROM Products", BeanPropertyRowMapper.newInstance(Products.class));
+    }
 }
