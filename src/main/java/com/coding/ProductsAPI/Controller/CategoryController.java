7198255260aac8d1362ea4c55962f333ebcc6a30
ImMael <legrand.mael2002@gmail.com>
2021-11-18 09:56:26 +0100

    CategoryController

--- src/main/java/com/coding/ProductsAPI/Controller/CategoryController.java
+++ src/main/java/com/coding/ProductsAPI/Controller/CategoryController.java
@@ -1,12 +1,60 @@
 package com.coding.ProductsAPI.Controller;
 
 import com.coding.ProductsAPI.DAO.CategoryDAO;
+import com.coding.ProductsAPI.models.Category;
+import com.coding.ProductsAPI.models.Products;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class CategoryController {
 
     @Autowired
     CategoryDAO categoryService;
+
+    @GetMapping("/category")
+    public @ResponseBody
+    List<Category> index(){
+        return categoryService.readAll();
+    }
+
+    @GetMapping("/category/{id}")
+    public @ResponseBody Category getProduct(@PathVariable("id") Integer id){
+        return categoryService.readById(id);
+    }
+
+    @PutMapping("/category/{id}")
+    public @ResponseBody
+    HttpStatus editProduct(@RequestBody Category body, @PathVariable("id") Integer id){
+        Integer res = categoryService.update(id, body);
+        if(res == 1){
+            return HttpStatus.OK;
+        } else{
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+    }
+    @DeleteMapping("/category/{id}")
+    public @ResponseBody
+    HttpStatus deleteProduct(@PathVariable("id") Integer id){
+        Integer res = categoryService.delete(id);
+        if(res == 1){
+            return HttpStatus.NO_CONTENT;
+        } else{
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+    }
+
+    @PostMapping("/category")
+    public @ResponseBody HttpStatus addProduct(@RequestBody Category body){
+        Integer res = categoryService.add(body);
+        if(res == 1){
+            return HttpStatus.CREATED;
+        } else{
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+    }
 }
--- src/main/java/com/coding/ProductsAPI/DAO/CategoryDAO.java
+++ src/main/java/com/coding/ProductsAPI/DAO/CategoryDAO.java
@@ -1,7 +1,41 @@
 package com.coding.ProductsAPI.DAO;
 
+import com.coding.ProductsAPI.models.Category;
+import com.coding.ProductsAPI.models.Products;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Category> readAll(){
+        return jdbcTemplate.query("SELECT * FROM Category", BeanPropertyRowMapper.newInstance(Category.class));
+    }
+
+    public Category readById(Integer id) {
+        String sql = "SELECT * FROM Category WHERE id=? ;";
+        return jdbcTemplate.queryForObject(sql, BeanPropertyRowMapper.newInstance(Category.class), id);
+    }
+
+    public int update(Integer id, Category cat) {
+        String sql = "UPDATE Category SET name = ? WHERE id = ?;";
+        return jdbcTemplate.update(sql, cat.getName(), id);
+    }
+
+    public int delete(Integer id){
+        String sql = "DELETE FROM Category WHERE id = ?;";
+        return jdbcTemplate.update(sql, id);
+    }
+    public int add(Category cat) {
+        String sql = "INSERT INTO Category (name) VALUES (?);";
+        return jdbcTemplate.update(sql, cat.getName());
+    }
+
 }
