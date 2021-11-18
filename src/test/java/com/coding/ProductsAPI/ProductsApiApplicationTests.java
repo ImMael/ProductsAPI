d50c5cf009e0d2adc4c4c53621af1e1e6ecd29b4
ImMael <legrand.mael2002@gmail.com>
2021-11-18 23:40:18 +0100

    Start Test unitaires

--- src/test/java/com/coding/ProductsAPI/ProductsApiApplicationTests.java
+++ src/test/java/com/coding/ProductsAPI/ProductsApiApplicationTests.java
@@ -1,13 +1,29 @@
 package com.coding.ProductsAPI;
 
+import com.coding.ProductsAPI.Controller.CategoryController;
+import com.coding.ProductsAPI.Controller.ProductsController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ProductsApiApplicationTests {
 
+	@Autowired
+	ProductsController productsController;
+
+	@Autowired
+	CategoryController categoryController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(productsController);
+		Assertions.assertNotNull(categoryController);
 	}
 
 }
